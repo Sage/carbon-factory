ee5bc429cb8e82a5a993c607ff56813f9737ecf6
Andrew Tait <andrew.tait@sage.com>
2015-10-13 09:35:41 +0100

    fixed bugs from 0.14 upgrade

--- src/tpl/main.js
+++ src/tpl/main.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Route } from 'react-router';
 import CarbonRoute from 'carbon/lib/utils/route';
 
-var Route = Router.Route;
-
 var routes = (
   <Route path="/" />
 );
