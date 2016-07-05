95a5453a41e4ab0972b8891ab127df1d186eca30
Sean Armstrong <sean.armstrong@sage.com>
2016-07-05 14:15:20 +0100

    update cli template for babel6

--- src/tpl/gulpfile.js
+++ src/tpl/gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
-var BuildTask = require('carbon-factory/lib/gulp/build');
-var SpecTask = require('carbon-factory/lib/gulp/spec');
+var BuildTask = require('carbon-factory/lib/gulp/build').default;
+var SpecTask = require('carbon-factory/lib/gulp/spec').default;
 
 gulp.task('default', BuildTask());
 
