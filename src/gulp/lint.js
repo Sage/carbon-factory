1b9dfd1cc11896ec080b87514c2f93ff31c3e321
Andrew Tait <andrew.tait@sage.com>
2017-07-26 17:03:38 +0100

    fixed up lint task

--- src/gulp/lint.js
+++ src/gulp/lint.js
@@ -5,14 +5,14 @@
  *
  *    import LintTask from 'carbon-factory/lib/gulp/lint';
  *
- *    gulp.task('lint', LintTask);
+ *    gulp.task('lint', LintTask());
  *
  * The lint task allows you to specify an errorThreshold (defaulted to 0) where the task will fail if the threshold is exceeded
  * Simply pass the errorThreshold to your gulp task:
  *
- *   gulp.task('lint', function() {
- *     return LintTask({ errorThreshold: 1 });
- *   });
+ *   gulp.task('lint', LintTask({
+ *     errorThreshold: 1
+ *   }));
  *
  * Other options:
  *
@@ -33,60 +33,63 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const yargs = require('yargs');
-const PluginError = require('gulp-util').PluginError;
 const gutil = require('gulp-util');
 const fs = require('fs');
 
 var argv = yargs.argv;
 
-export default function(opts) {
-  var options = opts || {};
+export default function(options) {
+  var opts = options;
 
-  var errorThreshold = options.eslintThreshold || options.errorThreshold || 0;
-  var warningThreshold = options.warningThreshold || 0;
-  // http://eslint.org/docs/user-guide/formatters/#output-examples
-  var format = options.format || 'stylish';
+  return function(done) {
+    var options = opts || {};
 
-  var path = options.path || 'src/**/!(__spec__|definition).js';
+    var errorThreshold = options.eslintThreshold || options.errorThreshold || 0;
+    var warningThreshold = options.warningThreshold || 0;
+    // http://eslint.org/docs/user-guide/formatters/#output-examples
+    var format = options.format || 'stylish';
 
-  fs.stat(process.cwd() + '/.eslintrc', function(err, stat) {
-    if (err == null) { return }
-    if (err.code == 'ENOENT') {
-      gutil.log(gutil.colors.red("Cannot find '.eslintrc' file"));
-      gutil.log(gutil.colors.white("Create an '.eslintrc' file in the root of your project and add the following code:\n{\n  \"extends\": \"./node_modules/carbon-factory/.eslintrc\"\n}"));
-      process.exit();
+    var path = options.path || 'src/**/!(__spec__|definition).js';
+
+    fs.stat(process.cwd() + '/.eslintrc', function(err, stat) {
+      if (err == null) { return }
+      if (err.code == 'ENOENT') {
+        gutil.log(gutil.colors.red("Cannot find '.eslintrc' file"));
+        gutil.log(gutil.colors.white("Create an '.eslintrc' file in the root of your project and add the following code:\n{\n  \"extends\": \"./node_modules/carbon-factory/.eslintrc\"\n}"));
+        process.exit();
+      }
+    });
+
+    var eslintTask = gulp.src([path], { base: process.cwd() })
+      .pipe(eslint({
+        configFile:  process.cwd() + '/.eslintrc'
+      }))
+      .pipe(eslint.format(format));
+
+    if (argv.build) {
+      eslintTask = eslintTask.pipe(eslint.results((results, callback) => {
+        var error, message = '';
+
+        if (errorThreshold < results.errorCount) {
+          error = true;
+          message += 'Error Count (' + results.errorCount + ') is greater than the threshold (' + errorThreshold + ')\n'
+        }
+
+        if (warningThreshold < results.warningCount) {
+          error = true;
+          message += 'Warning Count (' + results.warningCount + ') is greater than the threshold (' + warningThreshold + ')'
+        }
+
+        if (error) {
+          gutil.log(gutil.colors.red('ESLint Failed:\n\n' + message));
+          process.exit(1);
+        }
+
+        gutil.log(gutil.colors.green('ESLint Passed'));
+        callback();
+      }));
     }
-  });
 
-  var eslintTask = gulp.src([path], { base: process.cwd() })
-    .pipe(eslint({
-      configFile:  process.cwd() + '/.eslintrc'
-    }))
-    .pipe(eslint.format(format));
-
-  if (argv.build) {
-    return eslintTask.pipe(eslint.results((results, callback) => {
-      var error, message = '';
-
-      if (errorThreshold && errorThreshold < results.errorCount) {
-        error = true;
-        message += 'Error Count (' + results.errorCount + ') is greater than the threshold (' + errorThreshold + ')\n'
-      }
-
-      if (warningThreshold && warningThreshold < results.warningCount) {
-        error = true;
-        message += 'Warning Count (' + results.warningCount + ') is greater than the threshold (' + warningThreshold + ')'
-      }
-
-      if (error) {
-        throw new PluginError('Carbon Factory Lint', {
-          name: 'ESLint Threshold Error',
-          message: message
-        });
-      }
-      callback();
-    }));
-  } else {
     return eslintTask;
   }
 }
--- src/gulp/spec.js
+++ src/gulp/spec.js
@@ -10,7 +10,7 @@
  * You can also pass a hash of options to customise the task:
  *
  *    import SpecTask from 'carbon-factory/lib/gulp/spec';
- *
+ 
  *    var opts = {
  *      jestConfig: {
  *        watch: false
@@ -39,7 +39,6 @@ var lint = require('./lint').default;
 var gutil = require('gulp-util');
 var fs = require('fs');
 
-gulp.task('lint', lint);
 
 // Config Options https://facebook.github.io/jest/docs/configuration.html
 var baseJestConfig = {
@@ -48,30 +47,40 @@ var baseJestConfig = {
 
 var argv = yargs.argv;
 
-export default function(opts) {
-  // use custom config supplied via opts, or use our base config
-  var config = opts.jestConfig || baseJestConfig,
-      cliOptions = { watch: true, onlyChanged: true, config: config }
+export default function(options) {
+  var opts = options;
 
-  fs.stat(process.cwd() + '/.babelrc', function(err, stat) {
-    if (err == null) { return }
-    if (err.code == 'ENOENT') {
-      gutil.log(gutil.colors.red("Cannot find '.babelrc' file"));
-      gutil.log(gutil.colors.white("Create a '.babelrc' file in the root of your project and add the following code:\n{\n  \"extends\": \"./node_modules/carbon-factory/.babelrc\"\n}"));
-      process.exit();
-    }
-  });
-
-  if (argv.build) {
-    gulp.start('lint');
-    cliOptions = { config: config }
-  }
-
-  // TODO: Can we pass more arguments here to jestCli
   return function(done) {
+    // use custom config supplied via opts, or use our base config
+    var config = opts.jestConfig || baseJestConfig,
+        cliOptions = { watch: true, onlyChanged: true, config: config }
+
+    fs.stat(process.cwd() + '/.babelrc', function(err, stat) {
+      if (err == null) { return }
+      if (err.code == 'ENOENT') {
+        gutil.log(gutil.colors.red("Cannot find '.babelrc' file"));
+        gutil.log(gutil.colors.white("Create a '.babelrc' file in the root of your project and add the following code:\n{\n  \"extends\": \"./node_modules/carbon-factory/.babelrc\"\n}"));
+        process.exit();
+      }
+    });
+
+    if (argv.build) {
+      gulp.task('lint', lint(opts));
+      gulp.start('lint');
+      cliOptions = { config: config }
+    }
+
+    // TODO: Can we pass more arguments here to jestCli
     // https://github.com/facebook/jest/blob/master/packages/jest-cli/src/cli/index.js
-    jest.runCLI(cliOptions, '.', function() {
+    jest.runCLI(cliOptions, '.', function(results) {
       done();
+
+      if (results.success) {
+        gutil.log(gutil.colors.green('UI Tests SUCCEEDED'));
+      } else {
+        gutil.log(gutil.colors.red('UI Tests FAILED'));
+        process.exit(1);
+      }
     });
   }
 }
