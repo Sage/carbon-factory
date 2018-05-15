4f61c625a9dd7d14011beb376de8e13a881282f2
Andrew Tait <andrew.tait@sage.com>
2018-05-15 21:38:48 +0100

    added dirs for assets

--- webpack.config.js
+++ webpack.config.js
@@ -63,17 +63,19 @@ module.exports = function(opts) {
   /* Image Loader */
   const imageLoader = {
     test: /\.(png|svg|jpg|gif)$/,
-    use: [
-      'file-loader'
-    ]
+    use: [{
+      loader: 'file-loader',
+      outputPath: 'images/'
+    }]
   };
 
   /* Font Loader */
   const fontLoader = {
     test: /\.(woff|woff2|eot|ttf|otf)$/,
-    use: [
-      'file-loader'
-    ]
+    use: [{
+      loader: 'file-loader',
+      outputPath: 'fonts/'
+    }]
   };
 
   /* Webpack Config */
